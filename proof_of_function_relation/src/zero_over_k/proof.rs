e14341ded50c91eec6a07d62812fcb06463be4c0
Andrija <akinovak@gmail.com>
2022-05-25 19:58:22 +0200

    cleanup
    Co-authored-by: Nicolas Mohnblatt <nmohnblatt@users.noreply.github.com>

--- proof_of_function_relation/src/zero_over_k/proof.rs
+++ proof_of_function_relation/src/zero_over_k/proof.rs
@@ -1,20 +1,7 @@
 use crate::{
     commitment::HomomorphicPolynomialCommitment,
-    error::{to_pc_error, Error},
-    label_commitment, label_polynomial,
-    transcript::TranscriptProtocol,
-    util::{commit_polynomial, powers_of, shift_dense_poly},
-    virtual_oracle::VirtualOracle,
 };
-use ark_ff::{PrimeField, Zero};
-use ark_poly::{
-    univariate::{DenseOrSparsePolynomial, DensePolynomial},
-    EvaluationDomain, GeneralEvaluationDomain, Polynomial, UVPolynomial,
-};
-use ark_poly_commit::{LabeledPolynomial, PCRandomness};
-use ark_std::marker::PhantomData;
-use merlin::Transcript;
-use rand::Rng;
+use ark_ff::{PrimeField};
 
 pub struct Proof<F: PrimeField, PC: HomomorphicPolynomialCommitment<F>> {
     // commitments
