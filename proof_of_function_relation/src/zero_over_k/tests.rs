3f58da53f13db01100a6b97a9dbf48f6069ee74c
Koh Wei Jie <contact@kohweijie.com>
2022-06-20 10:41:43 +0800

    zero_over_k failure test for Check2Failed with a ProdVO

--- proof_of_function_relation/src/zero_over_k/tests.rs
+++ proof_of_function_relation/src/zero_over_k/tests.rs
@@ -3,8 +3,9 @@ mod test {
     use crate::{
         commitment::{HomomorphicPolynomialCommitment, KZG10},
         virtual_oracle::inverse_check_oracle::InverseCheckOracle,
+        virtual_oracle::prod_vo::ProdVO,
         zero_over_k::ZeroOverK,
-        error::{to_pc_error},
+        error::{to_pc_error, Error},
         label_polynomial,
     };
     use ark_bn254::{Bn254, Fr};
@@ -26,9 +27,9 @@ mod test {
     type D = Blake2s;
 
     // TODO
-    // Write tests. e.g. non_zero_over_k uses zero_over_k with an InverseCheckOracle.
-    // Perhaps try other virtual oracles like prod_vo too.
-    //
+    // Write more tests. e.g. non_zero_over_k uses zero_over_k with an InverseCheckOracle.
+    // Try other virtual oracles like add_vo.
+ 
     #[test]
     fn test_zero_over_k_inverse_check_oracle() {
         let n = 8;
@@ -93,6 +94,82 @@ mod test {
         );
             
         assert!(is_valid.is_ok());
+    }
+
+    #[test]
+    fn test_zero_over_k_prod_vo() {
+        let n = 8;
+        let mut rng = thread_rng();
+        let domain = GeneralEvaluationDomain::<F>::new(n).unwrap();
+
+        let max_degree = 20;
+        let pp = PC::setup(max_degree, None, &mut rng).unwrap();
+        let (ck, vk) = PC::trim(&pp, max_degree, 0, None).unwrap();
+
+        let f_evals: Vec<F> = vec![
+            F::from(1u64),
+            F::from(2u64),
+            F::from(3u64),
+            F::from(4u64),
+            F::from(5u64),
+            F::from(6u64),
+            F::from(7u64),
+            F::from(8u64),
+        ];
+
+        let f = DensePolynomial::<F>::from_coefficients_slice(&domain.ifft(&f_evals));
+        let f = label_polynomial!(f);
+
+        let g_evals: Vec<F> = vec![
+            F::from(1u64),
+            F::from(2u64),
+            F::from(3u64),
+            F::from(4u64),
+            F::from(5u64),
+            F::from(6u64),
+            F::from(7u64),
+            F::from(8u64),
+        ];
+
+        let g = DensePolynomial::<F>::from_coefficients_slice(&domain.ifft(&g_evals));
+        let g = LabeledPolynomial::new(String::from("g"), g.clone(), None, None);
+
+        let concrete_oracles = [f, g];
+        let alphas = vec![F::one(), F::one()];
+        let (commitments, rands) =
+            PC::commit(&ck, &concrete_oracles, None).map_err(to_pc_error::<F, PC>).unwrap();
+
+        let zero_over_k_vo = ProdVO {};
+
+        let zero_over_k_proof = ZeroOverK::<F, PC, D>::prove(
+            &concrete_oracles,
+            &commitments,
+            &rands,
+            &zero_over_k_vo,
+            &alphas,
+            &domain,
+            &ck,
+            &mut rng,
+        );
+
+        assert!(zero_over_k_proof.is_ok());
+
+        let is_valid = ZeroOverK::<F, PC, D>::verify(
+            zero_over_k_proof.unwrap(),
+            &commitments,
+            &zero_over_k_vo,
+            &domain,
+            &alphas,
+            &vk,
+        );
+            
+        assert!(is_valid.is_err());
+
+        // Test for a specific error
+        assert_eq!(
+            is_valid.err().unwrap(),
+            Error::Check2Failed
+        );
     }
 
     // fn test_zero_over_k_normalized_oracle() {
