b5c8723d9be07d53b1551fd2cbc04dc06877a275
Nicolas Mohnblatt <nmohnblatt@protonmail.com>
2022-06-28 17:01:22 +0100

    experimenting with degree bounds and linear combinations

--- proof_of_function_relation/src/commitment.rs
+++ proof_of_function_relation/src/commitment.rs
@@ -2,8 +2,9 @@
 use ark_ec::{msm::VariableBaseMSM, PairingEngine};
 use ark_ff::PrimeField;
 use ark_poly::univariate::DensePolynomial;
-use ark_poly_commit::PCRandomness;
-use ark_poly_commit::{sonic_pc::SonicKZG10, LabeledCommitment, PolynomialCommitment};
+use ark_poly_commit::{
+    sonic_pc::SonicKZG10, LabeledCommitment, PCRandomness, PolynomialCommitment,
+};
 
 /// A homomorphic polynomial commitment
 pub trait HomomorphicPolynomialCommitment<F>: PolynomialCommitment<F, DensePolynomial<F>>
@@ -68,5 +69,241 @@ where
         }
 
         acc
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::commitment::{HomomorphicPolynomialCommitment, KZG10};
+    use ark_bn254::{Bn254, Fr};
+    use ark_ff::One;
+    use ark_ff::UniformRand;
+    use ark_marlin::ahp::EvaluationsProvider;
+    use ark_poly::{
+        univariate::DensePolynomial, EvaluationDomain, Evaluations, GeneralEvaluationDomain,
+    };
+    use ark_poly_commit::LinearCombination;
+    use ark_poly_commit::QuerySet;
+    use ark_poly_commit::{
+        Evaluations as CommitEvaluations, LabeledCommitment, LabeledPolynomial,
+        PolynomialCommitment,
+    };
+    use ark_std::rand::thread_rng;
+    use rand_core::OsRng;
+    use std::iter;
+
+    type F = Fr;
+    type PC = KZG10<Bn254>;
+
+    fn generate_test_polynomials_with_degree_bounds() -> (
+        GeneralEvaluationDomain<F>,
+        LabeledPolynomial<F, DensePolynomial<F>>,
+        LabeledPolynomial<F, DensePolynomial<F>>,
+        LabeledPolynomial<F, DensePolynomial<F>>,
+    ) {
+        let n = 4;
+        let domain = GeneralEvaluationDomain::<F>::new(n).unwrap();
+
+        //test oracle to be zero at roots of unity
+        let a_evals = vec![F::from(2u64), F::from(4u64)];
+        let a_poly = Evaluations::from_vec_and_domain(a_evals, domain).interpolate();
+        let a_poly = LabeledPolynomial::new(String::from("a"), a_poly, Some(4), Some(1));
+
+        let b_evals = vec![
+            F::from(5u64),
+            F::from(4 as u64),
+            F::from(6 as u64),
+            F::from(8 as u64),
+        ];
+        let b_poly = Evaluations::from_vec_and_domain(b_evals, domain).interpolate();
+        let b_poly = LabeledPolynomial::new(String::from("b"), b_poly, Some(4), Some(1));
+
+        let a_plus_b_poly = a_poly.polynomial() + b_poly.polynomial();
+        let a_plus_b_poly =
+            LabeledPolynomial::new(String::from("a_plus_b"), a_plus_b_poly, Some(4), Some(1));
+
+        (domain, a_poly, b_poly, a_plus_b_poly)
+    }
+
+    fn generate_test_polynomials_no_degree_bounds() -> (
+        GeneralEvaluationDomain<F>,
+        LabeledPolynomial<F, DensePolynomial<F>>,
+        LabeledPolynomial<F, DensePolynomial<F>>,
+        LabeledPolynomial<F, DensePolynomial<F>>,
+    ) {
+        let n = 4;
+        let domain = GeneralEvaluationDomain::<F>::new(n).unwrap();
+
+        //test oracle to be zero at roots of unity
+        let a_evals = vec![F::from(2u64), F::from(4u64)];
+        let a_poly = Evaluations::from_vec_and_domain(a_evals, domain).interpolate();
+        let a_poly = LabeledPolynomial::new(String::from("a"), a_poly, None, Some(1));
+
+        let b_evals = vec![
+            F::from(5u64),
+            F::from(4 as u64),
+            F::from(6 as u64),
+            F::from(8 as u64),
+        ];
+        let b_poly = Evaluations::from_vec_and_domain(b_evals, domain).interpolate();
+        let b_poly = LabeledPolynomial::new(String::from("b"), b_poly, None, Some(1));
+
+        let a_plus_b_poly = a_poly.polynomial() + b_poly.polynomial();
+        let a_plus_b_poly =
+            LabeledPolynomial::new(String::from("a_plus_b"), a_plus_b_poly, None, Some(1));
+
+        (domain, a_poly, b_poly, a_plus_b_poly)
+    }
+
+    #[test]
+    fn test_commit_with_bounds() {
+        let rng = &mut thread_rng();
+        let maximum_degree: usize = 16;
+        let hiding_bound = 1;
+        let enforced_degree_bounds = [4];
+
+        let (_domain, a_poly, b_poly, a_plus_b_poly) =
+            generate_test_polynomials_with_degree_bounds();
+
+        let pp = PC::setup(maximum_degree, None, &mut OsRng).unwrap();
+        let (ck, vk) = PC::trim(
+            &pp,
+            maximum_degree,
+            hiding_bound,
+            Some(&enforced_degree_bounds),
+        )
+        .unwrap();
+
+        let (commitments, rands) = PC::commit(&ck, &[a_poly, b_poly], Some(rng)).unwrap();
+        let coefficients = vec![Fr::one(); commitments.len()];
+
+        let a_plus_b_commit = PC::multi_scalar_mul(&commitments, &coefficients);
+        let a_plus_b_commit =
+            LabeledCommitment::new(String::from("a_plus_b"), a_plus_b_commit, Some(4));
+
+        let challenge = Fr::rand(rng);
+
+        let evaluation_point = Fr::rand(rng);
+        let eval = a_plus_b_poly.evaluate(&evaluation_point);
+
+        let homomorphic_randomness = PC::aggregate_randomness(&rands);
+
+        let opening_proof = PC::open(
+            &ck,
+            &[a_plus_b_poly.clone()],
+            &[a_plus_b_commit.clone()],
+            &evaluation_point,
+            challenge,
+            &[homomorphic_randomness],
+            None,
+        )
+        .unwrap();
+
+        let res = PC::check(
+            &vk,
+            &[a_plus_b_commit],
+            &evaluation_point,
+            iter::once(eval),
+            &opening_proof,
+            challenge,
+            None,
+        );
+
+        println!("{:?}", res);
+    }
+
+    #[test]
+    // Note that we cannot use LinearCombination with polynomials that have degree bounds
+    fn test_linear_combinations() {
+        let rng = &mut thread_rng();
+
+        // Parameters
+        let maximum_degree: usize = 16;
+        let hiding_bound = 1;
+        let enforced_degree_bounds = [4];
+
+        // Setup the commitment scheme
+        let pp = PC::setup(maximum_degree, None, &mut OsRng).unwrap();
+        let (ck, vk) = PC::trim(
+            &pp,
+            maximum_degree,
+            hiding_bound,
+            Some(&enforced_degree_bounds),
+        )
+        .unwrap();
+
+        // Define polynomials and a linear combination
+        let (_domain, a_poly, b_poly, a_plus_b_poly) = generate_test_polynomials_no_degree_bounds();
+        let polynomials = vec![a_poly, b_poly];
+        let linear_combination =
+            LinearCombination::new("a_plus_b", vec![(F::one(), "a"), (F::one(), "b")]);
+        let linear_combinations = vec![linear_combination];
+
+        // Commit Phase
+        let (commitments, rands) = PC::commit(&ck, &polynomials, Some(rng)).unwrap();
+
+        // Derive evaluation point and generate a query set
+        let evaluation_point = Fr::rand(rng);
+        let mut query_set = QuerySet::new();
+        query_set.insert((
+            linear_combinations[0].label().clone(),
+            (String::from("challenge"), evaluation_point),
+        ));
+
+        for (poly_label, (point_label, point)) in &query_set {
+            println!(
+                "Query poly {} at point {}: {}",
+                poly_label, point_label, point
+            );
+        }
+
+        // Evaluation Phase, here we only output the evaluation of the linear combination
+        let manual_eval = a_plus_b_poly.evaluate(&evaluation_point);
+        let lc_eval = polynomials
+            .get_lc_eval(&linear_combinations[0], evaluation_point)
+            .unwrap();
+        assert_eq!(manual_eval, lc_eval);
+
+        let mut evaluations = CommitEvaluations::new();
+        evaluations.insert(
+            (linear_combinations[0].label().clone(), evaluation_point),
+            lc_eval,
+        );
+        for ((poly_label, point), eval) in &evaluations {
+            println!(
+                "Evaluated poly {} at point {}. Value is {}",
+                poly_label, point, eval
+            );
+        }
+
+        //Generate opening proof
+        let opening_challenge = Fr::rand(rng);
+        let opening_proof = PC::open_combinations(
+            &ck,
+            &linear_combinations,
+            &polynomials,
+            &commitments,
+            &query_set,
+            opening_challenge,
+            &rands,
+            Some(rng),
+        )
+        .unwrap();
+
+        // Verify opening proof
+        assert_eq!(
+            true,
+            PC::check_combinations(
+                &vk,
+                &linear_combinations,
+                &commitments,
+                &query_set,
+                &evaluations,
+                &opening_proof,
+                opening_challenge,
+                rng
+            )
+            .is_ok()
+        )
     }
 }
--- proof_of_function_relation/src/zero_over_k/tests.rs
+++ proof_of_function_relation/src/zero_over_k/tests.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod test {
     use crate::{
-        commitment::{HomomorphicPolynomialCommitment, KZG10},
+        commitment::KZG10,
         error::{to_pc_error, Error},
         label_polynomial,
         virtual_oracle::inverse_check_oracle::InverseCheckOracle,
@@ -9,19 +9,14 @@ mod test {
         zero_over_k::ZeroOverK,
     };
     use ark_bn254::{Bn254, Fr};
+    use ark_ff::Field;
     use ark_ff::One;
-    use ark_ff::{Field, UniformRand};
     use ark_poly::{
-        univariate::DensePolynomial, EvaluationDomain, Evaluations, GeneralEvaluationDomain,
-        UVPolynomial,
+        univariate::DensePolynomial, EvaluationDomain, GeneralEvaluationDomain, UVPolynomial,
     };
-    use ark_poly_commit::{
-        LabeledCommitment, LabeledPolynomial, PCRandomness, PolynomialCommitment,
-    };
+    use ark_poly_commit::{LabeledPolynomial, PolynomialCommitment};
     use ark_std::rand::thread_rng;
     use blake2::Blake2s;
-    use rand_core::OsRng;
-    use std::iter;
 
     type F = Fr;
     type PC = KZG10<Bn254>;
@@ -170,84 +165,5 @@ mod test {
 
         // Test for a specific error
         assert_eq!(is_valid.err().unwrap(), Error::Check2Failed);
-    }
-
-    #[test]
-    fn test_commit_with_bounds() {
-        let n = 4;
-        let domain = GeneralEvaluationDomain::<F>::new(n).unwrap();
-        let rng = &mut thread_rng();
-
-        //test oracle to be zero at roots of unity
-        let a_evals = vec![F::from(2u64), F::from(4u64)];
-        let a_poly = Evaluations::from_vec_and_domain(a_evals, domain).interpolate();
-        let a_poly = LabeledPolynomial::new(String::from("a"), a_poly, Some(4), None);
-
-        let b_evals = vec![
-            F::from(5u64),
-            F::from(4 as u64),
-            F::from(6 as u64),
-            F::from(8 as u64),
-        ];
-        let b_poly = Evaluations::from_vec_and_domain(b_evals, domain).interpolate();
-        let b_poly = LabeledPolynomial::new(String::from("b"), b_poly, Some(4), None);
-
-        let a_plus_b_poly = a_poly.polynomial() + b_poly.polynomial();
-        let a_plus_b_poly =
-            LabeledPolynomial::new(String::from("a_plus_b"), a_plus_b_poly, Some(4), None);
-
-        let point = Fr::rand(rng);
-        let eval = a_plus_b_poly.evaluate(&point);
-
-        let maximum_degree: usize = 16;
-
-        let pp = PC::setup(maximum_degree, None, &mut OsRng).unwrap();
-        let (ck, vk) = PC::trim(&pp, maximum_degree, 0, Some(&[4])).unwrap();
-
-        let (commitments, _rands) = PC::commit(&ck, &[a_poly, b_poly], None).unwrap();
-        let a_commit = commitments[0].clone();
-        //let a_rand = rands[0].clone();
-
-        let b_commit = commitments[1].clone();
-        // let b_rand = rands[1].clone();
-
-        let one = Fr::one();
-        let a_plus_b_commit = PC::multi_scalar_mul(&[a_commit, b_commit], &[one, one]);
-        let a_plus_b_commit =
-            LabeledCommitment::new(String::from("a_plus_b"), a_plus_b_commit, Some(4));
-
-        let challenge = Fr::from(1u64);
-
-        let homomorphic_randomness =
-            ark_poly_commit::kzg10::Randomness::<Fr, DensePolynomial<Fr>>::empty();
-
-        let opening = PC::open(
-            &ck,
-            &[a_plus_b_poly.clone()],
-            &[a_plus_b_commit.clone()],
-            &point,
-            challenge,
-            &[homomorphic_randomness],
-            None,
-        )
-        .unwrap();
-
-        let res = PC::check(
-            &vk,
-            &[a_plus_b_commit],
-            &point,
-            iter::once(eval),
-            &opening,
-            challenge,
-            None,
-        );
-
-        println!("{:?}", res);
-
-        // let (commitments, randoms) = PC::commit(&ck, &[a_poly_no_bound, a_poly_with_bound], None).unwrap();
-
-        // for c in commitments {
-        //     println!("{:?}", c.commitment());
-        // }
     }
 }
