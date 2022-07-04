94d4862c4d2f0bc82223a0e667f5e5395717cae0
Koh Wei Jie <contact@kohweijie.com>
2022-07-04 17:41:57 +0800

    refactored matrix to poly functions

--- ac2tft/src/lib.rs
+++ ac2tft/src/lib.rs
@@ -1,8 +1,23 @@
 mod tests;
 mod error;
+
 use std::fmt;
+use std::collections::BTreeMap;
 use ark_ff::PrimeField;
-use ark_marlin::ahp::indexer::Matrix;
+use ark_relations::r1cs::{
+    Matrix,
+    SynthesisError,
+};
+use ark_poly::{EvaluationDomain, GeneralEvaluationDomain};
+use ark_marlin::ahp::indexer::{
+    sum_matrices
+};
+
+use ark_marlin::ahp::constraint_systems::{
+    num_non_zero,
+    arithmetize_matrix,
+    MatrixArithmetization,
+};
 
 /// A compiler from arithmetic gates to t-FT sparse matrices, which can be converted into
 /// polynomicals using Marlin's `arithmetize_matrix()` function.
@@ -90,3 +105,107 @@ pub fn empty_matrix<F: PrimeField>(
     }
     matrix
 }
+
+pub fn gates_to_sparse_matrices<F: PrimeField>(
+    gates: Vec<Gate<F>>,
+) -> (
+    Matrix<F>,
+    Matrix<F>,
+    Matrix<F>,
+) {
+    let mut left_input_map = BTreeMap::<GateInput<F>, usize>::new();
+    let mut right_input_map = BTreeMap::<GateInput<F>, usize>::new();
+
+    //println!("Left inputs");
+    let mut j = 0;
+    for gate in gates.iter() {
+        if !left_input_map.contains_key(&gate.left) {
+            left_input_map.insert(gate.left.clone(), j);
+            //println!("{}, {}", j, gate.left);
+            j += 1;
+        }
+    }
+    //println!("");
+
+    j = 0;
+    //println!("Right inputs");
+    for gate in gates.iter() {
+        if !right_input_map.contains_key(&gate.right) {
+            right_input_map.insert(gate.right.clone(), j);
+            //println!("{}, {}", j, gate.right);
+            j += 1;
+        }
+    }
+
+    let n_i = left_input_map.len() + right_input_map.len();
+    let n_o = gates.len();
+    let matrix_width = n_i + n_o;
+
+    let mut matrix_a = empty_matrix::<F>(matrix_width);
+    let mut matrix_b = empty_matrix::<F>(matrix_width);
+    let mut matrix_c = empty_matrix::<F>(matrix_width);
+ 
+    // TODO: check for off-by-one errors here! the paper uses matrices which start from 1, but
+    // in Rust we start from 0.
+    for (i, gate) in gates.iter().enumerate() {
+        let row = n_i + i;
+        let l_i = *left_input_map.get(&gate.left).unwrap() + 1;
+        let r_i = *right_input_map.get(&gate.right).unwrap() + 1;
+
+        let col_a = match gate.symbol {
+            GateType::Add => 0,
+            GateType::Mul => l_i,
+        };
+
+        // Matrix A
+        matrix_a[row].push((F::from(1u64), col_a));
+
+        // Matrix B
+        if gate.symbol == GateType::Add {
+            if l_i != r_i {
+                matrix_b[row].push((F::from(1u64), l_i));
+            }
+        }
+        matrix_b[row].push((F::from(1u64), r_i));
+
+        // Matrix C
+        matrix_c[row].push((F::from(1u64), row));
+    }
+
+    (matrix_a, matrix_b, matrix_c)
+}
+
+pub fn sparse_matrices_to_polys<F: PrimeField>(
+    a: Matrix<F>,
+    b: Matrix<F>,
+    c: Matrix<F>,
+    num_constraints: usize,
+    num_formatted_input_variables: usize,
+    ) -> MatrixArithmetization<F> {
+    let mut a = a.clone();
+    let mut b = b.clone();
+    let mut c = c.clone();
+
+    let joint_matrix = sum_matrices(&a, &b, &c);
+    let num_non_zero_val = num_non_zero(&joint_matrix);
+
+    let domain_h = GeneralEvaluationDomain::<F>::new(num_constraints)
+        .ok_or(SynthesisError::PolynomialDegreeTooLarge).unwrap();
+    let domain_k = GeneralEvaluationDomain::<F>::new(num_non_zero_val)
+        .ok_or(SynthesisError::PolynomialDegreeTooLarge).unwrap();
+    let x_domain = GeneralEvaluationDomain::<F>::new(num_formatted_input_variables)
+        .ok_or(SynthesisError::PolynomialDegreeTooLarge).unwrap();
+
+    let joint_arith = arithmetize_matrix(
+        &joint_matrix,
+        &mut a,
+        &mut b,
+        &mut c,
+        domain_k,
+        domain_h,
+        x_domain,
+        );
+
+    joint_arith
+}
+
--- ac2tft/src/tests.rs
+++ ac2tft/src/tests.rs
@@ -1,23 +1,12 @@
 #[cfg(test)]
 mod tests {
-    use ark_relations::r1cs::{
-        Matrix,
-        SynthesisError,
-    };
-    use ark_poly::{EvaluationDomain, GeneralEvaluationDomain};
+    use ark_relations::r1cs::Matrix;
     use ark_poly::univariate::DensePolynomial;
     use ark_marlin::ahp::indexer::{
         sum_matrices
     };
-
-    use ark_marlin::ahp::constraint_systems::{
-        num_non_zero,
-        arithmetize_matrix,
-        MatrixArithmetization,
-    };
-    use std::collections::BTreeMap;
     use ark_bn254::Fr;
-    use crate::{GateType, GateInput, Gate, empty_matrix};
+    use crate::{GateType, GateInput, Gate, gates_to_sparse_matrices, sparse_matrices_to_polys};
     use crate::error::Error;
 
     type F = Fr;
@@ -42,156 +31,81 @@ mod tests {
     fn sample_gates_0() -> Vec<Gate<F>> {
         // Encodes x^3 + 2x + 5
         /*
-            TODO: rename these to g0 .. g4
-            g1: (x, x, *)
-            g2: (g1, x, *)
-            g3: (x, 2, *)
-            g4: (g2, g3, +)
-            g5: (g4, 5, +)
+            g0: (x, x, *)
+            g1: (g0, x, *)
+            g2: (x, 2, *)
+            g3: (g1, g2, +)
+            g4: (g3, 5, +)
 
             s: [*, *, *, +, +]
-            l: [x, g1, g2, g4]
-            r: [x, 2, g3, 5]
+            l: [x, g0, g1, g3]
+            r: [x, 2, g2, 5]
         */
 
-        let g1 = Gate::<Fr> {
+        let g0 = Gate::<Fr> {
             left: GateInput::Input(String::from("x")),
+            right: GateInput::Input(String::from("x")),
+            symbol: GateType::Mul,
+            label: String::from("g0"),
+        };
+
+        let g1 = Gate::<Fr> {
+            left: GateInput::Gate(Box::new(g0.clone())),
             right: GateInput::Input(String::from("x")),
             symbol: GateType::Mul,
             label: String::from("g1"),
         };
 
         let g2 = Gate::<Fr> {
-            left: GateInput::Gate(Box::new(g1.clone())),
-            right: GateInput::Input(String::from("x")),
+            left: GateInput::Input(String::from("x")),
+            right: GateInput::Constant(F::from(2u64)),
             symbol: GateType::Mul,
             label: String::from("g2"),
         };
 
         let g3 = Gate::<Fr> {
-            left: GateInput::Input(String::from("x")),
-            right: GateInput::Constant(F::from(2u64)),
-            symbol: GateType::Mul,
+            left: GateInput::Gate(Box::new(g1.clone())),
+            right: GateInput::Gate(Box::new(g2.clone())),
+            symbol: GateType::Add,
             label: String::from("g3"),
         };
 
         let g4 = Gate::<Fr> {
-            left: GateInput::Gate(Box::new(g2.clone())),
-            right: GateInput::Gate(Box::new(g3.clone())),
+            left: GateInput::Gate(Box::new(g3.clone())),
+            right: GateInput::Constant(F::from(5u64)),
             symbol: GateType::Add,
             label: String::from("g4"),
         };
 
-        let g5 = Gate::<Fr> {
-            left: GateInput::Gate(Box::new(g4.clone())),
-            right: GateInput::Constant(F::from(5u64)),
-            symbol: GateType::Add,
-            label: String::from("g5"),
-        };
-
-        return vec![g1, g2, g3, g4, g5];
+        return vec![g0, g1, g2, g3, g4];
     }
 
     #[test]
     fn test_gate_input_eq() {
         let gates = sample_gates_0();
-        let g1 = &gates[0];
-        let g2 = &gates[1];
-        let g3 = &gates[2];
-        assert_eq!(g1.left, g1.right);
-        assert_eq!(g1.left, g2.right);
-        assert_eq!(g3.right, GateInput::Constant(F::from(2u64)));
+        let g0 = &gates[0];
+        let g1 = &gates[1];
+        let g2 = &gates[2];
+        assert_eq!(g0.left, g0.right);
+        assert_eq!(g0.left, g1.right);
+        assert_eq!(g2.right, GateInput::Constant(F::from(2u64)));
     }
 
     #[test]
     fn test_into_gate() {
         let gates = sample_gates_0();
-        let g1 = &gates[0];
-        let g2 = &gates[1];
-        assert_eq!(g2.left.into_gate().unwrap(), *g1);
-        assert_eq!(g2.right.into_gate(), Err(Error::GateInputNotGate));
+        let g0 = &gates[0];
+        let g1 = &gates[1];
+        assert_eq!(g1.left.into_gate().unwrap(), *g0);
+        assert_eq!(g1.right.into_gate(), Err(Error::GateInputNotGate));
     }
 
     #[test]
     fn test_gates_to_matrices() {
         let gates = sample_gates_0();
 
-        println!("Gates from sample_gates_0");
-        for gate in gates.iter() {
-            println!("{}", gate);
-        }
-        println!("");
+        let (matrix_a, matrix_b, matrix_c) = gates_to_sparse_matrices(gates);
 
-
-        let mut left_input_map = BTreeMap::<GateInput<F>, usize>::new();
-        let mut right_input_map = BTreeMap::<GateInput<F>, usize>::new();
-
-        println!("Left inputs");
-        let mut j = 0;
-        for gate in gates.iter() {
-            //left_input_map.entry(gate.left.clone()).or_insert(i);
-            //right_input_map.entry(gate.right.clone()).or_insert(i);
-
-            if !left_input_map.contains_key(&gate.left) {
-                left_input_map.insert(gate.left.clone(), j);
-                println!("{}, {}", j, gate.left);
-                j += 1;
-            }
-        }
-        println!("");
-
-        j = 0;
-        println!("Right inputs");
-        for gate in gates.iter() {
-            if !right_input_map.contains_key(&gate.right) {
-                right_input_map.insert(gate.right.clone(), j);
-                println!("{}, {}", j, gate.right);
-                j += 1;
-            }
-        }
-        let n_g = gates.len();
-        let n_i = left_input_map.len() + right_input_map.len();
-        let n_o = gates.len();
-
-        assert_eq!(n_g, 5);
-        assert_eq!(n_i, 8);
-        assert_eq!(n_o, 5);
-
-        let matrix_width = n_i + n_o;
-        assert_eq!(matrix_width, 13);
-
-        let mut matrix_a = empty_matrix::<F>(matrix_width);
-        let mut matrix_b = empty_matrix::<F>(matrix_width);
-        let mut matrix_c = empty_matrix::<F>(matrix_width);
- 
-        // TODO: check for off-by-one errors here! the paper uses matrices which start from 1, but
-        // in Rust we start from 0.
-        for (i, gate) in gates.iter().enumerate() {
-            let row = n_i + i;
-            let l_i = *left_input_map.get(&gate.left).unwrap() + 1;
-            let r_i = *right_input_map.get(&gate.right).unwrap() + 1;
-
-            let col_a = match gate.symbol {
-                GateType::Add => 0,
-                GateType::Mul => l_i,
-            };
-
-            // Matrix A
-            matrix_a[row].push((F::from(1u64), col_a));
-            
-            // Matrix B
-            if gate.symbol == GateType::Add {
-                if l_i != r_i {
-                    matrix_b[row].push((F::from(1u64), l_i));
-                }
-            }
-            matrix_b[row].push((F::from(1u64), r_i));
-
-            // Matrix C
-            matrix_c[row].push((F::from(1u64), row));
-        }
-
-        println!("");
         print_matrix("matrix a", matrix_a.clone());
         print_matrix("matrix b", matrix_b.clone());
         print_matrix("matrix c", matrix_c.clone());
@@ -216,40 +130,10 @@ mod tests {
         print_polynomial("val_a", joint_arith.val_a.polynomial());
         print_polynomial("val_b", joint_arith.val_b.polynomial());
         print_polynomial("val_c", joint_arith.val_c.polynomial());
-    }
 
-    fn sparse_matrices_to_polys(
-        a: Matrix<Fr>,
-        b: Matrix<Fr>,
-        c: Matrix<Fr>,
-        num_constraints: usize,
-        num_formatted_input_variables: usize,
-    ) -> MatrixArithmetization<F> {
-        let mut a = a.clone();
-        let mut b = b.clone();
-        let mut c = c.clone();
-
-        let joint_matrix = sum_matrices(&a, &b, &c);
-        let num_non_zero_val = num_non_zero(&joint_matrix);
-
-        let domain_h = GeneralEvaluationDomain::<Fr>::new(num_constraints)
-            .ok_or(SynthesisError::PolynomialDegreeTooLarge).unwrap();
-        let domain_k = GeneralEvaluationDomain::<Fr>::new(num_non_zero_val)
-            .ok_or(SynthesisError::PolynomialDegreeTooLarge).unwrap();
-        let x_domain = GeneralEvaluationDomain::<Fr>::new(num_formatted_input_variables)
-            .ok_or(SynthesisError::PolynomialDegreeTooLarge).unwrap();
-
-        let joint_arith = arithmetize_matrix(
-            &joint_matrix,
-            &mut a,
-            &mut b,
-            &mut c,
-            domain_k,
-            domain_h,
-            x_domain,
-        );
-
-        joint_arith
+        // TODO:
+        // Apply t_slt test on matrix_a, matrix b
+        // Apply t_diag test on matrix_c
     }
 
     fn print_matrix(
