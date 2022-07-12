759aa4c10cec52027acbe38b517fff0104095576
Koh Wei Jie <contact@kohweijie.com>
2022-07-12 12:59:34 +0800

    print matrices for sample_circuit_0 and sample_gates_0

--- ac2tft/src/tests.rs
+++ ac2tft/src/tests.rs
@@ -1,9 +1,23 @@
 #[cfg(test)]
 pub mod tests {
-    use ark_relations::r1cs::Matrix;
     use ark_poly::univariate::DensePolynomial;
     use ark_bn254::Fr;
-    use crate::{GateType, GateInput, Gate};
+    use ark_ff::Field;
+    use ark_relations::{
+        lc,
+        r1cs::{
+            Variable,
+            ConstraintSystem,
+            OptimizationGoal,
+            ConstraintSynthesizer,
+            ConstraintSystemRef,
+            ConstraintMatrices,
+            SynthesisError,
+            Matrix,
+        },
+    };
+    //use ark_marlin::constraint_systems::make_matrices_square_for_indexer;
+    use crate::{GateType, GateInput, Gate, gates_to_sparse_matrices};
     use crate::error::Error;
 
     type F = Fr;
@@ -93,6 +107,107 @@ pub mod tests {
         let g1 = &gates[1];
         assert_eq!(g1.left.into_gate().unwrap(), *g0);
         assert_eq!(g1.right.into_gate(), Err(Error::GateInputNotGate));
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct SampleCircuit0<F: Field> {
+        x: Option<F>,
+        out: Option<F>,
+    }
+    
+    impl<F: Field> ConstraintSynthesizer<F> for SampleCircuit0<F> {
+        fn generate_constraints(
+            self,
+            cs: ConstraintSystemRef<F>,
+        ) -> Result<(), SynthesisError> {
+            // Encodes x^3 + 2x + 5
+            let x = cs.new_input_variable(|| self.x.ok_or(SynthesisError::AssignmentMissing))?;
+            let out = cs.new_input_variable(|| self.out.ok_or(SynthesisError::AssignmentMissing))?;
+
+            // x^3
+            let x2 = cs.new_witness_variable(|| {
+                let mut x = self.x.ok_or(SynthesisError::AssignmentMissing)?;
+                x.square_in_place();
+                Ok(x)
+            })?;
+
+            let x3 = cs.new_witness_variable(|| {
+                let x = self.x.ok_or(SynthesisError::AssignmentMissing)?;
+                let mut x2 = self.x.ok_or(SynthesisError::AssignmentMissing)?;
+                let mut x3 = self.x.ok_or(SynthesisError::AssignmentMissing)?;
+                x2.mul_assign(&x);
+                x3.mul_assign(&x2);
+                Ok(x3)
+            })?;
+            
+            // Enforce that x3 = x2 * x
+            cs.enforce_constraint(lc!() + x, lc!() + x2, lc!() + x3)?;
+
+            // x_double = 2x
+            let x_double = cs.new_witness_variable(|| {
+                let x = self.x.ok_or(SynthesisError::AssignmentMissing)?;
+                Ok(x * F::from(2u64))
+            })?;
+
+            // Enforce that x_double = 2 * x
+            cs.enforce_constraint(
+                lc!() + (F::from(2u64), Variable::One),
+                lc!() + x,
+                lc!() + x_double
+            )?;
+
+            // x_double_plus_5
+            let x_double_plus_5 = cs.new_witness_variable(|| {
+                let x = self.x.ok_or(SynthesisError::AssignmentMissing)?;
+                Ok(x * F::from(2u64) + F::from(5u64))
+            })?;
+
+            // Enforce that x_double_plus_5 = x_double_plus_5_w * 1
+            cs.enforce_constraint(
+                lc!() + (F::from(1u64), Variable::One),
+                lc!() + (F::from(1u64), x_double) + (F::from(5u64), Variable::One),
+                lc!() + x_double_plus_5
+            )?;
+
+            // out = x^3 + 2x + 5
+            cs.enforce_constraint(
+                lc!() + (F::from(1u64), Variable::One),
+                lc!() + (F::from(1u64), x3) + (F::from(1u64), x_double_plus_5),
+                lc!() + out
+            )?;
+
+            Ok(())
+        }
+    }
+
+    #[test]
+    pub fn test_sample_circuit_0() {
+        // Encodes x^3 + 2x + 5
+        let circ = SampleCircuit0 {
+            x: Some(Fr::from(1u64)),
+            out: Some(Fr::from(9u64))
+        };
+        let pcs = ConstraintSystem::new_ref();
+        pcs.set_optimization_goal(OptimizationGoal::Weight);
+        pcs.set_mode(ark_relations::r1cs::SynthesisMode::Prove {
+            construct_matrices: true,
+        });
+        let _ = circ.generate_constraints(pcs.clone());
+
+        // Requires importing a Marlin fork
+        //make_matrices_square_for_indexer(pcs.clone());
+
+        let constraint_matrices: ConstraintMatrices<Fr> = pcs.to_matrices().unwrap();
+
+        print_matrix("sample_circuit_0 matrix a", constraint_matrices.a);
+        print_matrix("sample_circuit_0 matrix b", constraint_matrices.b);
+        print_matrix("sample_circuit_0 matrix c", constraint_matrices.c);
+
+        let gates = sample_gates_0();
+        let matrices = gates_to_sparse_matrices(gates);
+        print_matrix("sample_gates_0 matrix a", matrices.0);
+        print_matrix("sample_gates_0 matrix b", matrices.1);
+        print_matrix("sample_gates_0 matrix c", matrices.2);
     }
 
     fn print_matrix(
