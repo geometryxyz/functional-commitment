d57bb39cb74c2a692b7e791b6612eef1d80f68d1
Andrija <akinovak@gmail.com>
2022-08-24 16:22:57 +0200

    Cleanup of homomorphic commitments
    Co-authored-by: Nicolas Mohnblatt <nmohnblatt@users.noreply.github.com>

--- homomorphic_poly_commit/src/lib.rs
+++ homomorphic_poly_commit/src/lib.rs
@@ -12,22 +12,7 @@ pub mod sonic_kzg;
 pub trait AdditivelyHomomorphicPCS<F>: PolynomialCommitment<F, DensePolynomial<F>>
 where
     F: PrimeField,
-    Self::VerifierKey: core::fmt::Debug,
 {
-    /// Compute the linear combination of the provided commitments
-    fn get_commitments_lc(
-        commitments: &[LabeledCommitment<Self::Commitment>],
-        lc: &LinearCombination<F>,
-    ) -> Result<LabeledCommitment<Self::Commitment>, Error>;
-
-    /// Compute the commitment and randomness that result of the linear combination of the provided commtiments and randomness values.
-    /// We assume that commitments and randomness match 1-to-1 and are in the same order.
-    fn get_commitments_lc_with_rands(
-        commitments: &[LabeledCommitment<Self::Commitment>],
-        hiding_rands: &[Self::Randomness],
-        lc: &LinearCombination<F>,
-    ) -> Result<(LabeledCommitment<Self::Commitment>, Self::Randomness), Error>;
-
     /// Aggregate labeled commitments according to the linear combination. If hiding bounds are enforced, the committer is expected to provide
     /// a vector of hiding randomness values, otherwise use `None`. A verifier can always aggregate with `None` for randomness.
     fn aggregate_commitments(
